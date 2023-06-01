f9a9e8b34a3c2dfd10e08ad4544825e319f92de8
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-02 08:27:03 +0900

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
