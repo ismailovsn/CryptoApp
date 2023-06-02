afce086bdd8e3f86bc520694aa08a733108f8475
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-02 14:47:56 +0900

    feat: add groups hierarchy and wrap all app with navigation view

---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleButtonView.swift
+//  CryptoApp
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/02.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonView()
+    }
+}
---
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  CryptoApp
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/02.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("header")
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
