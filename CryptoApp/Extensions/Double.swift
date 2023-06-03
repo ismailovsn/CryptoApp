80607cecfc05d0c936255c67f2be8abccb956e6c
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-03 17:31:52 +0900

    feat: add func to convert Double to string

--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+extension Double {
+    
+    /// Converts a Double into a currency with 2-6 decimal places
+    ///```
+    ///Convert 1234.56 to $1,234.56
+    ///Convert 12.3456 to $12.3456
+    ///Convert 0.123456 to $0.123456
+    ///```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "krw"
+//        formatter.currencySymbol = "₩"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    /// Converts a Double into a currency with 2-6 decimal places
+    ///```
+    ///Convert 1234.56 to "$1,234.56"
+    ///Convert 12.3456 to "$12.3456"
+    ///Convert 0.123456 to "$0.123456"
+    ///```
+    func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+}
