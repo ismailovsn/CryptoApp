02429a696e02eb8a8b47739bc6e9a5ef226c80ae
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-03 17:11:48 +0900

    feat: add example coin model with data

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/03.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
