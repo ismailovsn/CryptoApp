5f9b21319cd75026c38df7b651c15a1999f9906a
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-03 17:46:42 +0900

    feat: add func to convert double into string with % symbol

--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -37,4 +37,20 @@ extension Double {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "$0.00"
     }
+    
+    /// Converts a Double into string
+    ///```
+    ///Convert 1.23456 to "1.23"
+    ///```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// Converts a Double into string with % symbol
+    ///```
+    ///Convert 1.23456 to "$1.23%"
+    ///```
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
 }
