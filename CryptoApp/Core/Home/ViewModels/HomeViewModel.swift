852c5fc8dc3c62c284a96dc9582a7ecd5fea5061
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-04 12:28:22 +0900

    feat: add an example home view model

---
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  CryptoApp
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/04.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
