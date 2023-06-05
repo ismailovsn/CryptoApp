8a6b465c1cab81e217edfceadd7f05fcb1276b73
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-06 07:31:38 +0900

    feat: download coin images using combine, mvvm and networking layer

---
+++ CryptoApp/Services/CoinImageService.swift
@@ -0,0 +1,35 @@
+//
+//  CoinImageService.swift
+//  CryptoApp
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/05.
+//
+
+import Combine
+import Foundation
+import SwiftUI
+
+class CoinImageService {
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
