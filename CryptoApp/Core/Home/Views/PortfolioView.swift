b60eaefc513475ffa70ee6d8e7a9e9663637b910
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-08 11:38:35 +0900

    feat: create portfolio view

--- CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct PortfolioView: View {
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject private var vm: HomeViewModel
+    
+    @State private var selectedCoin: CoinModel? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    SearchBarView(searchText: $vm.searchText)
+                    coinLogoList
+                    
+                }
+            }
+            .navigationTitle("Edit Portfolio")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.headline)
+                    }
+
+                }
+            }
+        }
     }
 }
 
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
+    }
+}
+
+extension PortfolioView {
+    var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id
+                                        ? Color.theme.green
+                                        : Color.clear
+                                        , lineWidth: 1)
+                        )
+                }
+            }
+            .padding(.vertical, 4)
+            .padding(.leading)
+        }
     }
 }
