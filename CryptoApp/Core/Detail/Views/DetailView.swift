66ee3182db8419fdf8fcb4e04e95e46b3bee8948
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-13 15:29:46 +0900

    feat: show coin symbol and image in detail view

--- CryptoApp/Core/Detail/Views/DetailView.swift
+++ CryptoApp/Core/Detail/Views/DetailView.swift
@@ -50,6 +50,11 @@ struct DetailView: View {
             .padding()
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navBarTrailingItem
+            }
+        }
     }
 }
 
@@ -62,6 +67,17 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 extension DetailView {
+    private var navBarTrailingItem: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+            .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+
+    }
+    
     private var overviewTitle: some View {
         Text("Overview")
             .font(.title)
